99215366b1bef89eb69a4ec5f866363875adb064
dewey <dban7171@gmail.com>
2025-04-23 14:34:13 +0900

    edit contract

--- backend/app/contracts/ChatContract.sol
+++ backend/app/contracts/ChatContract.sol
@@ -2,18 +2,17 @@
 pragma solidity ^0.8.17;
 
 contract ChatContract {
-    mapping(address => string[]) public chatRecords;
+    mapping(address => string[]) private chatRecords;
 
     event ChatStored(address indexed user, string ipfsHash);
 
-    // 본인만 저장 가능하게 수정
-    function storeChat(string memory ipfsHash) public {
-        require(bytes(ipfsHash).length < 256, "IPFS hash too long"); // 제한 추가
+    function storeChat(string memory ipfsHash) external {
+        require(bytes(ipfsHash).length < 256, "IPFS hash too long");
         chatRecords[msg.sender].push(ipfsHash);
         emit ChatStored(msg.sender, ipfsHash);
     }
 
-    function getChats(address user) public view returns (string[] memory) {
+    function getChats(address user) external view returns (string[] memory) {
         return chatRecords[user];
     }
 }
